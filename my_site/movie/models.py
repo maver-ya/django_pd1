05379902dea84ad7f6824bb596630543446d673d
maver-ya <maver2005@ya.ru>
2024-11-11 20:54:16 +0300

    fulfill db (V5)

--- my_site/movie/models.py
+++ my_site/movie/models.py
@@ -11,3 +11,5 @@ class Movie(models.Model):
     rating = models.DecimalField(max_digits=3, decimal_places=1, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.title
